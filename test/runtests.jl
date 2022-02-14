daf2143859509af4ca6686a8e17da3b372dd6669
Yang YC <yang.yc.allium@gmail.com>
2022-02-14 13:51:45 +0800

    Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,9 @@ env = [1 1; 1 2; 1 3; 2 1; 2 2; 2 3; 3 1; 3 2; 3 3]
                 # matrix of environmental coordinates
 field = MikrubiField(cty, geo, env)
 model = fit(field, ["a", "c", "f"])
-                # maximized log-likeliness: -4.3886
-model.pr_cell   # 0.6281, 0.4981, 0.3643, 0.4634, 0.3337, ...
-model.pr_county # "a"=>0.6281, "b"=>0.4981, "c"=>0.3643, ...
+                # maximized log-likeliness: -4.389
+model.pr_pixel  # 0.628, 0.498, 0.364, 0.463, 0.334, 0.222 ...
+model.pr_county # "a" => 0.628, "b" => 0.498, "c" => 0.364 ...
 sample(model)   # ["a", "d", "f"] in a random trial
-@test isapprox(sum(model.pr_cell), 3.137251f0)
+@test isapprox(sum(model.pr_pixel), 3.137251f0)
 @test isapprox(sum(model.params), -0.7130561f0)
